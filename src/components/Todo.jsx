990801a745d4619ce784be2228538c5072eb34d7
DRIV32 <rafifpermana39@gmail.com>
2024-12-30 07:21:37 +0700

    add input

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import task from "../assets/task.png";
 import TodoItems from "./TodoItems";
 
 const Todo = () => {
+  const [todoList, setTodoList] = useState([]);
+
+  const inputRef = useRef();
+
+  const add = () => {
+    const inputText = inputRef.current.value.trim();
+
+    if (inputText === "") {
+      return null;
+    }
+    const newTodo = {
+      id: Date.now(),
+      text: inputText,
+      isComplete: false,
+    };
+
+    setTodoList((prev) => [...prev, newTodo]);
+    inputRef.current.value = "";
+  };
   return (
     <div
       className="bg-white place-self-center w-11/12 max-w-md flex flex-col
@@ -15,17 +34,23 @@ const Todo = () => {
 
       <div className="flex items-center my-7 bg-gray-300 rounded-full">
         <input
+          ref={inputRef}
           className="bg-transparent border-none outline-none flex-1 h-14 pl-6 pr-2 placeholder:text-slate-900"
           type="text"
           placeholder="Add Task"
         />
-        <button className="border-none rounded-full bg-blue-600 w-32 h-14 text-white font-medium cursor-pointer">
+        <button
+          onClick={add}
+          className="border-none rounded-full bg-blue-600 w-32 h-14 text-white font-medium cursor-pointer"
+        >
           ADD +
         </button>
       </div>
 
       <div>
-        <TodoItems />
+        {todoList.map((item, index) => {
+          return <TodoItems key={index} text={item.text} />;
+        })}
       </div>
     </div>
   );
